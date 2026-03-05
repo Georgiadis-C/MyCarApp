cf35d1f4d382272f652349570160e1399d2a16bc
user <chrisg.e.o.rgiadis420@gmail.com>
2026-03-05 16:31:00 +0200

    Redesign car list UI and add Car query property
    Revamped CarsPage.xaml with a modern gradient background, improved car item layout, and enhanced styling for car details. Updated CarKmViewModel to use [QueryProperty] for Car and added an observable CarsModel property for navigation binding. No functional changes to toolbar logic.

--- MyCarApp/ViewModels/CarKmViewModel.cs
+++ MyCarApp/ViewModels/CarKmViewModel.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 using CommunityToolkit.Mvvm.ComponentModel;
+using MyCarApp.Models;
 
 namespace MyCarApp.ViewModels
 {
+    [QueryProperty(nameof(Car), "Car")]
     public partial class CarKmViewModel : ObservableObject
     {
-
-
+        [ObservableProperty]
+        CarsModel car;
     }
 }
